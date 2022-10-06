ee0c67d9ccc3c1ad91b7a574067403823f4843ec
damin <damin0320@gmail.com>
2022-10-06 10:44:09 +0900

    ADD : 테스트 초안 작성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react"
 
 function App() {
+  const todoInitialState = {title: ""}
+  const [todo, setTodo] = useState(todoInitialState)
+  const [todoList, setTodoList] = useState([{
+    id : 0,
+    title : '운동',
+  },
+  {
+    id : 1,
+    title : '공부',
+  },
+  {
+    id: 2,
+    title : '면접',
+  }
+])
+
+const handleChangeInput = (event) =>{
+  setTodo( {
+    ...todo,
+   [event.target.name] : event.target.value,
+  })
+}
+
+const handleAddTodo = () => {
+  setTodoList([...todoList, {...todo, id : todoList.length}])
+  setTodo(todoInitialState)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="header">
+      <input type="text" name="title" onChange={handleChangeInput}/>
+      <button type="button" onClick={handleAddTodo}>추가하기</button>
+      <h1>Todo List</h1>
+      </div>
+      <div className="todolist">
+        {todoList.map((todo) => {
+          return <p key={todo.id}>{todo.title}</p>
+      })}
+      </div>
+      <div>
+
+      </div>
+
     </div>
   );
 }
